141a56d7a52587436a6aed4478021c590960187e
Lewinskie <wangaralewis@gmail.com>
2024-08-19 10:45:47 +0300

    initial commit

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: "next",
-  rules: {
-    // Your custom rules here
-  },
-};
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Optional: Enforce strict mode in React
+  reactStrictMode: true,
 };
 
 export default nextConfig;
